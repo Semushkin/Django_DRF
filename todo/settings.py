13ca1769ba30f18265e0413264f586de48e7a305
anton <rkzton@yandex.ru>
2022-08-31 00:55:19 +0300

    Lesson_8

--- todo/settings.py
+++ todo/settings.py
@@ -152,11 +152,14 @@ REST_FRAMEWORK = {
     #'PAGE_SIZE': 100
 
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser'
+        #'rest_framework.permissions.IsAdminUser'
+        #'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        #'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
